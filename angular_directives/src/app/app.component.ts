c6ddac19918a909e9c536a31eda3ec3772f0f499
Kevyn Gorito <kevy4job@gmail.com>
2022-04-22 12:16:24 -0300

    Entendendo o conceito de ngFor e utilizando objetos para iterar no html

--- angular_directives/src/app/app.component.ts
+++ angular_directives/src/app/app.component.ts
@@ -8,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   count = 0;
 
+  pessoas = [
+    {
+      nome:"Ivonaldo",
+      sobrenome: "Soares"
+    },
+    {
+      nome:"Maria",
+      sobrenome: "Silva"
+    },
+    {
+      nome:"Ronaldo",
+      sobrenome: "Santana"
+    },
+    {
+      nome:"Tadeu",
+      sobrenome: "Smith"
+    }
+  ];
+
   constructor() {
 
   }
 
   ngOnInit () {
+    console.log(this.pessoas)
     let interval = setInterval(() => {
       this.count++;
       if(this.count == 10) {
