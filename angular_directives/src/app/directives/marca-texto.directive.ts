471368da40162e27571b97fc0301d26795445b9c
Kevyn Gorito <kevy4job@gmail.com>
2022-04-22 23:00:54 -0300

    Iniciando estudo sobre SPA

---
+++ angular_directives/src/app/directives/marca-texto.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appMarcaTexto]'
+})
+export class MarcaTextoDirective implements OnInit{
+  @Input () corDeFundo: string = 'yellow';
+  @Input () corDoTexto: string = 'white';
+
+  constructor(private elemento: ElementRef) { }
+
+  ngOnInit(){ 
+    this.mudarFundo();
+    
+  }
+
+  private mudarFundo(cor: string = 'yellow') {
+    this.elemento.nativeElement.style.backgroundcolor = this.corDeFundo || cor;
+    this.elemento.nativeElement.style.color = this.corDoTexto;
+    this.elemento.nativeElement.style.fontweight = 'bold';
+  }
+
+}
+
+
+
+
